f8dcfd76d49c3ccca94708e8fd474972a146c360
Maciej Bielecki <zyla@prati.pl>
2020-03-21 07:47:20 +0100

    Switch to tokio locks, fix build, fix infinite loop bug in find_victim

--- buffer-pool/src/lib.rs
+++ buffer-pool/src/lib.rs
@@ -6,7 +6,7 @@ use tokio::fs;
 use tokio::prelude::*;
 use std::path::Path;
 use std::collections::{HashMap};
-use std::sync::{RwLock};
+use tokio::sync::{RwLock};
 use crate::disk_manager::*;
 use std::sync::atomic::{AtomicUsize, AtomicBool, Ordering};
 use std::ops::{Deref, DerefMut};
@@ -81,22 +81,22 @@ impl BufferPool {
     }
 
     pub async fn get_page<'a>(&'a self, page_id: PageId) -> io::Result<PinnedPage<'a>> {
-        let inner = self.lock.read().unwrap();
+        let inner = self.lock.read().await;
         match inner.page_table.get(&page_id) {
             Some(&frame_id) => {
                 unlock(inner);
-                self.pin_existing_page(frame_id)
+                self.pin_existing_page(frame_id).await
             }
             None => {
                 unlock(inner);
 
-                let mut inner = self.lock.write().unwrap();
+                let mut inner = self.lock.write().await;
 
                 // Somebody else may have fetched the same page before we got the writer lock,
                 // in which case we're done.
                 if let Some(&frame_id) = inner.page_table.get(&page_id) {
                     unlock(inner);
-                    return self.pin_existing_page(frame_id);
+                    return self.pin_existing_page(frame_id).await;
                 }
 
                 let frame_id = self.get_free_frame(inner.deref_mut()).await?;
@@ -113,7 +113,7 @@ impl BufferPool {
                 // TODO: Should we still hold the lock while we're doing IO?
                 // A: Yes, but maybe a different one? (we shouldn't block reading existing tables,
                 // but we don't want to read the same page twice)
-                inner.disk_manager.read_page(page_id, page.data.get_mut().unwrap()).await?;
+                inner.disk_manager.read_page(page_id, page.data.get_mut()).await?;
 
                 inner.page_table.insert(page_id, frame_id);
 
@@ -130,12 +130,12 @@ impl BufferPool {
     }
 
     // May lock `inner` in write mode.
-    fn pin_existing_page<'a>(&'a self, frame_id: FrameId) -> io::Result<PinnedPage<'a>> {
+    async fn pin_existing_page<'a>(&'a self, frame_id: FrameId) -> io::Result<PinnedPage<'a>> {
         let page = &self.frames[frame_id];
         let old_pin_count = page.pin_count.fetch_add(1, Ordering::SeqCst);
 
-        if(old_pin_count == 0) {
-            let mut inner = self.lock.write().unwrap();
+        if old_pin_count == 0 {
+            let mut inner = self.lock.write().await;
 
             // Do we really need to take the writer lock here? Seems insane!
             // TODO: Measure if we can get some improvement if we have a different lock, or use a
@@ -148,7 +148,7 @@ impl BufferPool {
 
     // TODO: decopypaste - get_page
     pub async fn allocate_page<'a>(&'a self) -> io::Result<PinnedPage<'a>> {
-        let mut inner = self.lock.write().unwrap();
+        let mut inner = self.lock.write().await;
         let frame_id = self.get_free_frame(inner.deref_mut()).await?;
         let page_ = &self.frames[frame_id];
 
@@ -161,6 +161,12 @@ impl BufferPool {
         page.id = page_id;
         *page.dirty.get_mut() = false;
         *page.pin_count.get_mut() = 1;
+        
+        // Zero-fill the newly created page
+        let data = page.data.get_mut();
+        for i in data.iter_mut() {
+            *i = 0;
+        }
 
         inner.page_table.insert(page_id, frame_id);
 
@@ -183,7 +189,16 @@ impl BufferPool {
                 // - nobody could have increased it, because pin_count only increases while holding
                 // writer lock on `inner`.
                 let page = unsafe { &mut *(&self.frames[frame_id] as *const Page as *mut Page) };
-                panic!("No free frames");
+
+                if *page.dirty.get_mut() {
+                    inner.disk_manager.write_page(page.id, page.data.get_mut()).await?;
+                    *page.dirty.get_mut() = false;
+                }
+
+                page.id = PageId(std::usize::MAX);
+                *page.pin_count.get_mut() = 0;
+
+                Ok(frame_id)
             }
         }
     }
@@ -194,15 +209,15 @@ impl BufferPool {
         // On the second pass we're guaranteed to get a page, since we unrefed them all in the first pass.
         let mut i = 0;
         while i < self.capacity * 2 {
-            if self.frames[inner.clock_hand].pin_count.load(Ordering::SeqCst) > 0 {
-                debug!("find_victim: skip {}", inner.clock_hand);
-                continue;
-            }
-            if inner.ref_flag.get(inner.clock_hand) {
-                debug!("find_victim: unref {}", inner.clock_hand);
-                inner.ref_flag.set(inner.clock_hand, false);
+            if self.frames[inner.clock_hand].pin_count.load(Ordering::SeqCst) == 0 {
+                if inner.ref_flag.get(inner.clock_hand) == Some(&true) {
+                    println!("find_victim: unref {}", inner.clock_hand);
+                    inner.ref_flag.set(inner.clock_hand, false);
+                } else {
+                    return inner.clock_hand;
+                }
             } else {
-                return inner.clock_hand;
+                println!("find_victim: skip {}", inner.clock_hand);
             }
             i += 1;
             inner.clock_hand = (inner.clock_hand + 1) % self.capacity;
@@ -211,7 +226,7 @@ impl BufferPool {
     }
 }
 
-fn unlock<T>(lock: T) {}
+fn unlock<T>(_lock: T) {}
 
 #[cfg(test)]
 mod tests {
@@ -219,7 +234,7 @@ mod tests {
 
     async fn with_temp_db<R, RF: Future<Output = io::Result<R>>, F: FnOnce(DiskManager) -> RF>(f: F) -> io::Result<R> {
         let filename = "test.db";
-        let mut disk_manager = DiskManager::open(filename).await?;
+        let disk_manager = DiskManager::open(filename).await?;
         let result = f(disk_manager).await;
         fs::remove_file(filename).await?;
         result
@@ -228,14 +243,25 @@ mod tests {
     #[tokio::test]
     async fn test_allocate_and_read_one_page() {
         with_temp_db(|disk_manager| async {
-            let mut buffer_pool = BufferPool::new(disk_manager, 1);
+            let buffer_pool = BufferPool::new(disk_manager, 1);
             let page = buffer_pool.allocate_page().await?;
             assert_eq!(page.id, PageId(0));
-            page.data.write().unwrap()[0] = 5;
+            page.data.write().await[0] = 5;
 
             let page = buffer_pool.get_page(PageId(0)).await?;
-            assert_eq!(page.data.read().unwrap()[0], 5);
+            assert_eq!(page.data.read().await[0], 5);
 
+            Ok(())
+        }).await.unwrap()
+    }
+
+    #[tokio::test(basic_scheduler)]
+    #[should_panic] // TODO: change the panic to an error Result
+    async fn test_allocate_more_pages_than_capacity() {
+        with_temp_db(|disk_manager| async {
+            let buffer_pool = BufferPool::new(disk_manager, 1);
+            let _page1 = buffer_pool.allocate_page().await?;
+            let _page2 = buffer_pool.allocate_page().await?;
             Ok(())
         }).await.unwrap()
     }
