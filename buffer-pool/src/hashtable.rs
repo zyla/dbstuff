b6d8a94b69da1ba1f8662af4fc764a47d18f1d18
Maciej Bielecki <zyla@prati.pl>
2020-08-14 23:59:31 +0200

    Use InsertError instead of panic when table is full

--- buffer-pool/src/hashtable.rs
+++ buffer-pool/src/hashtable.rs
@@ -34,16 +34,25 @@ impl<K: Data, V: Data, H: Default + Hasher> HashTable<K, V, H> {
     }
 }
 
+#[derive(PartialEq, Eq, Debug)]
+pub enum InsertError<V> {
+    AlreadyExists(V),
+    TableFull,
+}
+
 impl<K: Data, V: Data, H: Hasher> HashTable<K, V, H> {
     pub fn with_capacity_and_hasher(capacity: usize, hasher: H) -> Self {
         assert!(is_power_of_2(capacity), "capacity not a power of two");
         Self {
-            data: (0..capacity).map(|_| Entry::<K, V>::empty()).collect::<Vec<_>>().into_boxed_slice(),
-            hasher
+            data: (0..capacity)
+                .map(|_| Entry::<K, V>::empty())
+                .collect::<Vec<_>>()
+                .into_boxed_slice(),
+            hasher,
         }
     }
 
-    pub fn insert(&self, key: K, value: V) -> Result<(), V> {
+    pub fn insert(&self, key: K, value: V) -> Result<(), InsertError<V>> {
         let h = self.hasher.hash(key.to_u64());
         let mut index = self.hash_to_index(h);
         let mut num_tries = 0;
@@ -51,36 +60,46 @@ impl<K: Data, V: Data, H: Hasher> HashTable<K, V, H> {
             let entry = &self.data[index];
             let k = entry.key.load(Ordering::SeqCst);
             if k == K::sentinel().to_u64() {
-                if entry.key.compare_exchange_weak(k, key.to_u64(), Ordering::SeqCst, Ordering::SeqCst).is_err() {
+                if entry
+                    .key
+                    .compare_exchange_weak(k, key.to_u64(), Ordering::SeqCst, Ordering::SeqCst)
+                    .is_err()
+                {
                     // Someone already inserted something here - try again
                     continue;
                 }
-                match entry.value.compare_exchange_weak(V::sentinel().to_u64(), value.to_u64(), Ordering::SeqCst, Ordering::SeqCst) {
-                    Ok(_) => {
-                        return Ok(())
-                    },
+                match entry.value.compare_exchange_weak(
+                    V::sentinel().to_u64(),
+                    value.to_u64(),
+                    Ordering::SeqCst,
+                    Ordering::SeqCst,
+                ) {
+                    Ok(_) => return Ok(()),
                     Err(existing_value) => {
                         // Someone raced with us and inserted something else into the "deleted
                         // entry"
-                        return Err(V::from_u64(existing_value));
+                        return Err(InsertError::AlreadyExists(V::from_u64(existing_value)));
                     }
                 }
             } else if k == key.to_u64() {
                 // No overwrites - only insert if it's empty
-                match entry.value.compare_exchange_weak(V::sentinel().to_u64(), value.to_u64(), Ordering::SeqCst, Ordering::SeqCst) {
-                    Ok(_) => {
-                        return Ok(())
-                    },
+                match entry.value.compare_exchange_weak(
+                    V::sentinel().to_u64(),
+                    value.to_u64(),
+                    Ordering::SeqCst,
+                    Ordering::SeqCst,
+                ) {
+                    Ok(_) => return Ok(()),
                     Err(existing_value) => {
                         // Someone raced with us and inserted something else into the "deleted
                         // entry"
-                        return Err(V::from_u64(existing_value));
+                        return Err(InsertError::AlreadyExists(V::from_u64(existing_value)));
                     }
                 }
             } else {
                 num_tries += 1;
                 if num_tries > self.data.len() {
-                    panic!("Hash table full");
+                    return Err(InsertError::TableFull);
                 }
                 index = (index + 1) & (self.data.len() - 1);
             }
@@ -102,10 +121,15 @@ impl<K: Data, V: Data, H: Hasher> HashTable<K, V, H> {
                 if v == V::sentinel().to_u64() {
                     return None;
                 }
-                match entry.value.compare_exchange_weak(v, V::sentinel().to_u64(), Ordering::SeqCst, Ordering::SeqCst) {
+                match entry.value.compare_exchange_weak(
+                    v,
+                    V::sentinel().to_u64(),
+                    Ordering::SeqCst,
+                    Ordering::SeqCst,
+                ) {
                     Ok(_) => {
                         return Some(V::from_u64(v));
-                    },
+                    }
                     Err(_) => {
                         // Something changed - recheck
                         continue;
@@ -171,7 +195,7 @@ impl Hasher for FNV1 {
     fn hash(&self, x: u64) -> u64 {
         let mut h: u64 = 0xcbf29ce484222325;
         for i in 0..8 {
-            h = (h ^ ((x >> i * 8) & 0xff)).wrapping_mul(0x100000001b3);
+            h = (h ^ ((x >> (i * 8)) & 0xff)).wrapping_mul(0x100000001b3);
         }
         h
     }
@@ -180,6 +204,7 @@ impl Hasher for FNV1 {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use InsertError::*;
 
     // Identity hash for testing
     #[derive(Default)]
@@ -209,7 +234,7 @@ mod tests {
     #[test]
     fn test_whitebox_insert_1() {
         let table = HashTable::<X, X, TestHash>::with_capacity(8);
-        assert_eq!(table.insert(X(1), X(100)), Ok(())); 
+        assert_eq!(table.insert(X(1), X(100)), Ok(()));
         assert_eq!(table.data[0].key.load(Ordering::SeqCst), 0);
         assert_eq!(table.data[0].value.load(Ordering::SeqCst), 0);
         assert_eq!(table.data[1].key.load(Ordering::SeqCst), 1);
@@ -221,8 +246,8 @@ mod tests {
     #[test]
     fn test_whitebox_insert_collision() {
         let table = HashTable::<X, X, TestHash>::with_capacity(8);
-        assert_eq!(table.insert(X(1), X(100)), Ok(())); 
-        assert_eq!(table.insert(X(0x101), X(101)), Ok(())); 
+        assert_eq!(table.insert(X(1), X(100)), Ok(()));
+        assert_eq!(table.insert(X(0x101), X(101)), Ok(()));
         assert_eq!(table.data[0].key.load(Ordering::SeqCst), 0);
         assert_eq!(table.data[0].value.load(Ordering::SeqCst), 0);
         assert_eq!(table.data[1].key.load(Ordering::SeqCst), 1);
@@ -234,7 +259,7 @@ mod tests {
     #[test]
     fn test_whitebox_delete_1() {
         let table = HashTable::<X, X, TestHash>::with_capacity(8);
-        assert_eq!(table.insert(X(1), X(100)), Ok(())); 
+        assert_eq!(table.insert(X(1), X(100)), Ok(()));
         assert_eq!(table.delete(X(1)), Some(X(100)));
         assert_eq!(table.data[0].key.load(Ordering::SeqCst), 0);
         assert_eq!(table.data[0].value.load(Ordering::SeqCst), 0);
@@ -253,30 +278,35 @@ mod tests {
     #[test]
     fn test_insert_not_exists() {
         let table = HashTable::<X, X, TestHash>::with_capacity(8);
-        assert_eq!(table.insert(X(1), X(10)), Ok(())); 
+        assert_eq!(table.insert(X(1), X(10)), Ok(()));
         assert_eq!(table.lookup(X(1)), Some(X(10)));
     }
 
     #[test]
     fn test_insert_exists() {
         let table = HashTable::<X, X, TestHash>::with_capacity(8);
-        assert_eq!(table.insert(X(1), X(10)), Ok(())); 
-        assert_eq!(table.insert(X(1), X(12)), Err(X(10))); 
+        assert_eq!(table.insert(X(1), X(10)), Ok(()));
+        assert_eq!(table.insert(X(1), X(12)), Err(AlreadyExists(X(10))));
         assert_eq!(table.lookup(X(1)), Some(X(10)));
     }
 
     #[test]
     fn test_lookup_not_exists_collision() {
         let table = HashTable::<X, X, TestHash>::with_capacity(8);
-        assert_eq!(table.insert(X(1), X(10)), Ok(())); 
+        assert_eq!(table.insert(X(1), X(10)), Ok(()));
         assert_eq!(table.lookup(X(0x101)), None);
     }
 
     #[test]
     fn test_insert_delete_lookup() {
         let table = HashTable::<X, X, TestHash>::with_capacity(8);
-        assert_eq!(table.insert(X(1), X(10)), Ok(())); 
-        assert_eq!(table.delete(X(1)), Some(X(10))); 
+        assert_eq!(table.insert(X(1), X(10)), Ok(()));
+        assert_eq!(table.delete(X(1)), Some(X(10)));
         assert_eq!(table.lookup(X(1)), None);
+    }
+
+    #[test]
+    fn test_threaded_insert_lookup() {
+        // TODO
     }
 }
