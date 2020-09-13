75e33aa8d29eedbfc8dc144d0429a4e959de4ec3
Maciej Bielecki <zyla@prati.pl>
2020-09-13 12:20:56 +0200

    Add a benchmark

---
+++ chan/benches/mpsc_bench.rs
@@ -0,0 +1,31 @@
+#![feature(test)]
+extern crate test;
+
+use ::chan::mpsc;
+use std::thread;
+
+#[bench]
+fn send_recv_unbounded(b: &mut test::bench::Bencher) {
+    b.iter(|| {
+        let (tx, rx) = mpsc::channel();
+        std::thread::spawn(move || {
+            for i in 1..=1000 {
+                tx.send(i);
+            }
+        });
+        while rx.recv() < 1000 {}
+    })
+}
+
+#[bench]
+fn send_recv_bounded(b: &mut test::bench::Bencher) {
+    b.iter(|| {
+        let (tx, rx) = mpsc::bounded_channel(100);
+        std::thread::spawn(move || {
+            for i in 1..=1000 {
+                tx.send(i);
+            }
+        });
+        while rx.recv() < 1000 {}
+    })
+}
